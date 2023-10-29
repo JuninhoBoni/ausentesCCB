7a17407e11d5da8ce900e06354e86fb0c0239d06
JuninhoBoni <juninhoboni@gmail.com>
2023-10-29 14:51:07 -0300

    Em prod

--- ausentesCCB/routes/front.py
+++ ausentesCCB/routes/front.py
@@ -59,7 +59,6 @@ async def insert(request: Request):
     client_id = payload.get("client_id")
     if sub == client_id:
         form = await request.form()
-        name = form.get('name', None)
         justificativa = form.get('justificativa', None)
         outraJustificativa = form.get('outraJustificativa', None)
 
@@ -76,45 +75,75 @@ async def insert(request: Request):
         if ("-admin" in sub and "-admin" in client_id) or (current_datetime <= date_hour_test):
             date_test = datetime(year, month, day, 0, 0, 0, tzinfo=brazil_tz)
 
-            document = {
-                "name": name,
-                "justificativa": justificativa,
-                "outraJustificativa": outraJustificativa,
-                "date": date_test,
-                "nameFind": unidecode(name).lower(),
-                "dateFind": date_test.strftime('%d-%m-%Y'),
-            }
+            cargo = form.get('cargo', None)
+            church = form.get('church', None)
+            church_db = await db['churches'].find_one({"praying_house": church})
 
-            # Verifique se já existe um documento com os campos 'nameFind' e 'dateFind'
-            existing_doc = await collection.find_one({"nameFind": document["nameFind"], "dateFind": document["dateFind"]})
-
-            if existing_doc:
-                # Se o documento já existe, atualize-o
-                collection.update_one(
-                    {"_id": existing_doc["_id"]},  # Use o _id para identificar o documento existente
-                    {"$set": document}  # Use $set para atualizar os campos
-                )
-                success = f"O nome {name} foi atualizado com sucesso no dia {date}!"
+            if cargo == 'Encarregado Local':
+                name = church_db['maestro']
+            elif cargo == 'Encarregado Regional':
+                name = church_db['maestro_master']
+            elif cargo == 'Examinadora':
+                name = church_db.get('examinadora', None)
             else:
-                # Se o documento não existe, insira-o
-                collection.insert_one(document)
-                success = f"O nome {name} foi inserido com sucesso no dia {date}!"
+                name = None
 
-            response = templatesAusentesManager.TemplateResponse("/justificativa/justificativa.html",
-                                                                 {
-                                                                     "request": request,
-                                                                     "title": f"Ausentes CCB",
-                                                                     "date": date,
-                                                                     "success": success
-                                                                 })
-            if 'BEARER' in access_token.upper():
-                access_token = access_token.split(' ')[-1]
-            response.set_cookie(key="Authorization", value=f"Bearer {access_token}", httponly=True, max_age=1800)
+            if name in (None, '***', '-'):
+                response = await justificativa_page(date, request, "", "Não foi possível encontrar o nome e ministério nesta comum!")
+            else:
+                document = {
+                    "name": name,
+                    "justificativa": justificativa,
+                    "outraJustificativa": outraJustificativa,
+                    "date": date_test,
+                    "nameFind": unidecode(name).lower(),
+                    "dateFind": date_test.strftime('%d-%m-%Y'),
+                }
+
+                # Verifique se já existe um documento com os campos 'nameFind' e 'dateFind'
+                existing_doc = await collection.find_one({"nameFind": document["nameFind"], "dateFind": document["dateFind"]})
+
+                if existing_doc:
+                    # Se o documento já existe, atualize-o
+                    collection.update_one(
+                        {"_id": existing_doc["_id"]},  # Use o _id para identificar o documento existente
+                        {"$set": document}  # Use $set para atualizar os campos
+                    )
+                    success = f"O nome {name} foi atualizado com sucesso no dia {date}!"
+                else:
+                    # Se o documento não existe, insira-o
+                    collection.insert_one(document)
+                    success = f"O nome {name} foi inserido com sucesso no dia {date}!"
+
+                response = await justificativa_page(date, request, success)
+                if 'BEARER' in access_token.upper():
+                    access_token = access_token.split(' ')[-1]
+                response.set_cookie(key="Authorization", value=f"Bearer {access_token}", httponly=True, max_age=1800)
         else:
             error = "Só é possível justificar até as 15:00h do sábado!"
             response = return_default(request, error)
         return response
     raise HTTPException(status_code=400, detail="ACESSO INVÁLIDO.")
+
+
+async def justificativa_page(date, request, success, error=''):
+    projection = {'_id': 1, 'praying_house': 1}
+
+    churches = []
+    async for document in db['churches'].find({}, projection).sort('praying_house', 1):
+        if document['praying_house'] != '***':
+            churches.append(document)
+
+    response = templatesAusentesManager.TemplateResponse("/justificativa/justificativa.html",
+                                                         {
+                                                             "request": request,
+                                                             "title": f"Ausentes CCB",
+                                                             "date": date,
+                                                             "success": success,
+                                                             "churches": churches,
+                                                             "error": error
+                                                         })
+    return response
 
 
 @router.post("/login_ausentes", response_class=HTMLResponse, include_in_schema=False)
@@ -135,12 +164,8 @@ async def login_for_access_token(request: Request):
         if lower_limit <= current_datetime <= date_hour_test:
             generate_uuid = generate_password(8)
             access_token = await create_access_token(data={"sub": generate_uuid}, expires_delta=access_token_expires)
-            response = templatesAusentesManager.TemplateResponse("/justificativa/justificativa.html",
-                                                                 {
-                                                                     "request": request,
-                                                                     "title": f"Ausentes CCB",
-                                                                     "date": username
-                                                                 })
+            response = await justificativa_page(username, request, "")
+
             if 'BEARER' in access_token.upper():
                 access_token = access_token.split(' ')[-1]
             response.set_cookie(key="Authorization", value=f"Bearer {access_token}", httponly=True, max_age=1800)
@@ -150,12 +175,8 @@ async def login_for_access_token(request: Request):
     elif password.lower() == 'admin':
         generate_uuid = f'{generate_password(8)}-admin'
         access_token = await create_access_token(data={"sub": generate_uuid}, expires_delta=access_token_expires)
-        response = templatesAusentesManager.TemplateResponse("/justificativa/justificativa.html",
-                                                             {
-                                                                 "request": request,
-                                                                 "title": f"Ausentes CCB - Acesso ADMIN",
-                                                                 "date": username
-                                                             })
+        response = await justificativa_page(username, request, "")
+
         if 'BEARER' in access_token.upper():
             access_token = access_token.split(' ')[-1]
         response.set_cookie(key="Authorization", value=f"Bearer {access_token}", httponly=True, max_age=1800)
